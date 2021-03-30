b0b5270725082fc7a2a4e6e191896b5eaca2fa0b
BohdanShmalko <43369238+Sapp3465@users.noreply.github.com>
2021-03-30 08:02:59 +0300

    lab 3.1 complete

--- labs3/lab3.1/Lab31.tsx
+++ labs3/lab3.1/Lab31.tsx
@@ -1,11 +1,24 @@
-import React, {FC} from "react";
-import {Text, View, ViewStyle, StyleSheet, Dimensions, TouchableOpacity, ImageBackground} from "react-native";
+import React, {FC, useState} from "react";
+import {
+    Text,
+    View,
+    ViewStyle,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    ImageBackground,
+    TextInput, TextStyle
+} from "react-native";
 import {NavigationType} from "../App";
+import {parts} from "./parts";
 
 interface Styles {
     mainContainer: ViewStyle,
-    button : ViewStyle,
-    whiteContainer : ViewStyle
+    button: ViewStyle,
+    whiteContainer: ViewStyle,
+    ok: ViewStyle,
+    labContainer: ViewStyle,
+    input: TextStyle
 }
 
 const stl = StyleSheet.create<Styles>({
@@ -17,22 +30,52 @@ const stl = StyleSheet.create<Styles>({
     button: {
         backgroundColor: '#42f563',
         marginBottom: 20,
-        paddingHorizontal : 150,
+        paddingHorizontal: 150,
         padding: 10,
         borderRadius: 10
     },
-    whiteContainer : {
+    whiteContainer: {
         backgroundColor: 'rgba(255,255,255,0.7)',
+    },
+    ok: {
+        backgroundColor: '#42f563',
+        marginBottom: 20,
+        paddingHorizontal: 20,
+        padding: 10,
+        borderRadius: 10
+    },
+    labContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    input: {
+        fontSize: 17,
+        backgroundColor: 'white',
+        borderWidth: 2,
+        borderColor: 'black',
+        padding: 4,
+        width: 100
     }
 })
 
 export const Lab31: FC<{ setNavigation: React.Dispatch<React.SetStateAction<NavigationType>> }> = ({setNavigation}) => {
+    const [n, changeN] = useState('')
+    const [result, changeResult] = useState('')
     return <ImageBackground style={stl.mainContainer}
                             source={{uri: 'http://2.bp.blogspot.com/-zQYxVjs7LDI/U2uJONTdKgI/AAAAAAAAAYQ/PoLKoXux_08/s1600/df.jpg'}}>
         <View style={stl.whiteContainer}>
-        <TouchableOpacity style={stl.button} onPress={() => setNavigation('Home')}>
-            <Text>To home</Text>
-        </TouchableOpacity>
+            <TouchableOpacity style={stl.button} onPress={() => setNavigation('Home')}>
+                <Text>To home</Text>
+            </TouchableOpacity>
+            <View style={stl.labContainer}>
+                <TextInput style={stl.input} keyboardType={'numeric'} placeholder={'n'} value={n}
+                           onChangeText={changeN}/>
+
+                <TouchableOpacity style={stl.ok} onPress={() => changeResult(parts(Number(n)))}>
+                    <Text>Ok</Text>
+                </TouchableOpacity>
+                <Text>{result}</Text>
+            </View>
         </View>
     </ImageBackground>
 
--- labs3/lab3.1/parts.ts
+++ labs3/lab3.1/parts.ts
@@ -1,1 +1,12 @@
-//const parts : ()
+export const parts : (n : number) => string = (n ) => {
+    if (n % 2 === 0) return 'p = 2, q = ' + n/2
+    let b, a = Math.ceil(Math.sqrt(n))
+    if (a * a === n) return 'p = ' + a + ' q = ' + a
+    while (true) {
+        let potential_answer = a * a - n;
+        b = Math.ceil(Math.sqrt(potential_answer))
+        if (b * b === potential_answer) break
+        a++
+    }
+    return 'p = ' + (a - b) + ' q = ' + (a + b)
+}
